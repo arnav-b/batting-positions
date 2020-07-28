f9d55de1dd96db758c50ecc073c849c75d870991
Arnav Burudgunte <ab141@rice.edu>
2020-07-28 15:05:37 -0500

    Implement class innings using pandas read_html.

--- match.py
+++ match.py
@@ -5,56 +5,57 @@ Created on Sun Jul 26 18:59:34 2020
 @author: arnav
 """
 
-import requests
-from bs4 import BeautifulSoup
 import pandas as pd
+
+pd.options.mode.chained_assignment = None
 
 class Match(object):
     
     def __init__(self, url):
         self.url = url
-        self.soup = BeautifulSoup(requests.get(url).content, 'lxml')
-        self.batting_tables = self.soup.find_all('table', attrs = {'class':'table batsman'})
-        self.bowling_tables = self.soup.find_all('table', attrs = {'class': 'table bowler'})
+        self.dfs = pd.read_html(url)
+    
+    def get_batting_df(self, innings):
+        df = self.dfs[2 * (innings - 1)]
+        df = df[df['BATSMEN'].notna()]  
         
-    def get_batting_table(self, innings):
-        return self.batting_tables[innings - 1]
-    
-    def get_bowling_table(self, innings):
-        return self.bowling_tables[innings - 1]
-    
-    def get_team(self, teamnum):
-        tableBatsmen = self.get_batting_table(teamnum)
-        team = []
-        for b in tableBatsmen.find_all('a'):
-            team.append(b.contents[0])
-        return team
-    
-    def get_venue(self):
-        venue = self.soup.find('td', attrs = {'class':'font-weight-bold match-venue'})
-        return venue.a.contents[0]
-    
-    def get_batting_scorecard(self, innings):
-          df = pd.read_html()
-        
-        
+
 url = 'https://www.espncricinfo.com/ci/engine/match/1152839.html'
-url2 = 'https://stats.espncricinfo.com/ci/engine/match/1225248.html'
-soup = BeautifulSoup(requests.get(url2).content, 'lxml')
-# details = soup.find('td', attrs = {'class':'font-weight-bold match-venue'})
-# print(details.a.contents[0])
-toss = soup.find('td', attrs = {'class':'font-weight-bold border-right'})
-print(toss)
-# tablesBatsmen = soup.find_all('table', attrs = {'class':'table batsman'})
-# firstInns = tablesBatsmen[1].find_all('a')
-# for b in firstInns:
-#     print(b.contents[0])
+url2 = 'https://stats.espncricinfo.com/ci/engine/match/1187016.html'
 
-match = Match(url)
-# print(match.get_team(1))
-# print(match.get_batting_table(1).prettify())
-t = match.get_batting_table(1)
-# dfs = pd.read_html(url)
+dfs = pd.read_html(url)
+df = dfs[0]
+df = df[df['BATSMEN'].notna()]
+df.drop(['Unnamed: 8', 'Unnamed: 9'], 1, inplace=True)
+df.drop(list(range(12,15)))
+df.index = list(range(1,15))
+# print(df)
 
-# df = dfs[0]
-# df = df[df['BATSMEN'].notna()]
+dfs2 = pd.read_html(url2)
+print(dfs2[5])
+
+class Innings(object):
+    
+    def __init__(self, battingdf, bowlingdf):
+        self.batdf = battingdf
+        self.bowldf = bowlingdf
+    
+    def get_bat_df(self):
+        df = self.batdf[self.batdf['BATSMEN'].notna()]
+        df.drop(['Unnamed: 8', 'Unnamed: 9'], 1, inplace=True)
+        df.drop(list(range(12,15)), inplace=True)
+        df.index = list(range(1,15))
+        return df
+    
+    def get_bowl_df(self):
+        return self.bowldf
+    
+    def get_total(self):
+        return int(self.battingdf.iat[12,2])
+    
+    def get_extras(self):
+        return int(self.battingdf.iat[11,2])
+    
+    def get_fow(self):
+        fow = self.battingdf.iat[13,0]
+        return fow
