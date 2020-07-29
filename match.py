c9c759a787c19071c4da7100e45011e34530b435
Arnav Burudgunte <ab141@rice.edu>
2020-07-28 19:14:27 -0500

    Redo class Match with pd.read_html function.
    Batsman dataframe functionality incomplete.

--- match.py
+++ match.py
@@ -13,48 +13,56 @@ class Match(object):
     
     def __init__(self, url):
         self.url = url
-        self.soup = BeautifulSoup(requests.get(url).content, 'lxml')
-        self.batting_tables = self.soup.find_all('table', attrs = {'class':'table batsman'})
-        self.bowling_tables = self.soup.find_all('table', attrs = {'class': 'table bowler'})
+        self.r = requests.get(url)
+        self.soup = BeautifulSoup(self.r.content, 'lxml')
+        self.bat_dfs = pd.read_html(self.r.content, attrs = {'class':'table batsman'})
+        self.bowl_dfs = pd.read_html(self.r.content, attrs = {'class':'table bowler'})
         
-    def get_batting_table(self, innings):
-        return self.batting_tables[innings - 1]
+    # def get_batting_table(self, innings):
+    #     try:
+    #         return self.batting_tables[innings - 1]
+    #     except IndexError:
+    #         raise IndexError('Innings does not exist')
     
-    def get_bowling_table(self, innings):
-        return self.bowling_tables[innings - 1]
+    def get_batting_df(self, innings):
+        df = pd.read_html(self.r.content, attrs = {'class':'table batsman'})
+        
     
-    def get_team(self, teamnum):
-        tableBatsmen = self.get_batting_table(teamnum)
-        team = []
-        for b in tableBatsmen.find_all('a'):
-            team.append(b.contents[0])
-        return team
+    def get_bowling_df(self, innings):
+        return pd.read_html(self.r.content, attrs = {'class':'table bowler'})[innings - 1]
+    
+    ## This function doesn't work
+    # def get_batting_team(self, innings):
+    #     tableBatsmen = self.get_batting_table(innings)
+    #     team = []
+    #     for b in tableBatsmen.find_all('a'):
+    #         team.append(b.contents[0])
+    #     return team
     
     def get_venue(self):
         venue = self.soup.find('td', attrs = {'class':'font-weight-bold match-venue'})
         return venue.a.contents[0]
     
-    def get_batting_scorecard(self, innings):
-          df = pd.read_html()
+    
         
         
-url = 'https://www.espncricinfo.com/ci/engine/match/1152839.html'
-url2 = 'https://stats.espncricinfo.com/ci/engine/match/1225248.html'
-soup = BeautifulSoup(requests.get(url2).content, 'lxml')
-# details = soup.find('td', attrs = {'class':'font-weight-bold match-venue'})
-# print(details.a.contents[0])
-toss = soup.find('td', attrs = {'class':'font-weight-bold border-right'})
-print(toss)
-# tablesBatsmen = soup.find_all('table', attrs = {'class':'table batsman'})
-# firstInns = tablesBatsmen[1].find_all('a')
-# for b in firstInns:
-#     print(b.contents[0])
+url_win = 'https://www.espncricinfo.com/ci/engine/match/1152839.html'
+url_inns_win = 'https://stats.espncricinfo.com/ci/engine/match/1187008.html'
+url_draw = 'https://stats.espncricinfo.com/ci/engine/match/1187672.html'
 
-match = Match(url)
-# print(match.get_team(1))
-# print(match.get_batting_table(1).prettify())
-t = match.get_batting_table(1)
-# dfs = pd.read_html(url)
+match_win = Match(url_win)
+match_inns_win = Match(url_inns_win)
+match_draw = Match(url_draw)
 
-# df = dfs[0]
-# df = df[df['BATSMEN'].notna()]
+dfs = pd.read_html(match_draw.r.content, attrs = {'class':'table batsman'})
+df1 = dfs[0]
+df2 = dfs[2]
+
+# print(len(match_inns_win.batting_tables))
+# batting_table = match_inns_win.get_batting_table(1)
+# for b in batting_table.find_all('a'):
+#     print(b.contents)
+# bowling_table = match_inns_win.get_bowling_table(1)
+
+    
+
