dadb8ef229ead0f2316c8bf7a2d7dbac187366ab
Arnav Burudgunte <ab141@rice.edu>
2020-08-05 00:41:10 -0500

    Start analysis on Smith. Add .csv file to repo.

--- analysis.py
+++ analysis.py
@@ -14,30 +14,25 @@ from math import floor
 
 # Get data
 
-df3 = pd.read_csv('df3.csv')
-df4 = pd.read_csv('df4.csv')
-
-df3.drop('Unnamed: 0', axis=1, inplace=True)
-df4.drop('Unnamed: 0', axis=1, inplace=True)
-
-
+df = pd.read_csv('df.csv')
 
 # Overall distribution of fall of wickets 
 
 fig, ax = plt.subplots()
 ax.set_title('Fall of Wicket Distribution (Runs)')
-sns.kdeplot(df3.FoWR, ax=ax, label='First Wicket')
-sns.kdeplot(df4.FoWR, ax=ax, label='Second Wicket')
+sns.kdeplot(df.FoWR1, ax=ax, label='First Wicket')
+sns.kdeplot(df.FoWR2, ax=ax, label='Second Wicket')
 
 fig, ax = plt.subplots()
 ax.set_title('Fall of Wicket Distribution (Overs)')
-sns.kdeplot(df3.FoWO, ax=ax, label='First Wicket')
-sns.kdeplot(df4.FoWO, ax=ax, label='Second Wicket')
+sns.kdeplot(df.FoWO1, ax=ax, label='First Wicket')
+sns.kdeplot(df.FoWO2, ax=ax, label='Second Wicket')
 
-# fig, ax = plt.subplots()
-# ax.set_title('Fall of Wicket Distribution (Minutes)')
-# sns.kdeplot(df3.FoWM, ax=ax, label='First Wicket')
-# sns.kdeplot(df4.FoWM, ax=ax, label='Second Wicket')
+fig, ax = plt.subplots()
+ax.set_title('Partnership for First vs Second Wicket')
+ax.set_xlabel('Opening Partnership (R)')
+ax.set_ylabel('Second Wicket Parternship (R)')
+sns.jointplot(x=df.FoWR1, y=df.FoWR2 - df.FoWR1, kind='scatter')
 
 def get_fow_runs(df, innings='all'):
     if innings != 'all':
@@ -53,8 +48,36 @@ def get_fow_overs(df, innings='all'):
 
 # Some scatterplots
 
-# sns.jointplot(x=df3.FoWR, y=df3.Total, kind='scatter')
+fig, ax = plt.subplots()
+ax.set_title('Second Wicket vs Opening Partnership')
+sns.scatterplot(x='FoWR1', y=df.apply(lambda x: x['FoWR2'] - x['FoWR1'], axis=1), 
+                hue='Innings', ax=ax, data=df)
+ax.set_xlabel('Opening Partnership')
+ax.set_ylabel('Second Wicket Partnership')
 
+fig, ax = plt.subplots()
+ax.set_title('No 3 Score vs Opening Partnership')
+sns.scatterplot(x='FoWR1', y='3R', hue='Innings', ax=ax, data=df)
+ax.set_xlabel('Fall of First Wicket (R)')
+ax.set_ylabel('No 3 Score')
+
+fig, ax = plt.subplots()
+ax.set_title('No 4 Score vs Fall of Second Wicket')
+sns.scatterplot(x='FoWR2', y='4R', hue='Innings', ax=ax, data=df)
+ax.set_xlabel('Fall of Second Wicket (R)')
+ax.set_ylabel('No 4 Score')
+
+# Analysis for Smith 
+
+dfSmith = df[(df['3Batsman'].str.contains('SPD Smith')) |\
+             (df['4Batsman'].str.contains('SPD Smith'))]
+    
+dfSmith['SmithPosition'] = dfSmith['3Batsman'].apply(lambda x: 3 if 'SPD Smith' in x else 4)
+    
+sns.catplot(x=dfSmith.SmithPosition.apply(lambda x: 'SPD Smith' if x == 3 else 'Others'),
+            y='3R', hue='Innings', kind='box', data=dfSmith)   
+plt.title('Smith vs Others at No 3')
+    
 # Averages by innings
 
 def get_average(df, innings):
--- cricinfo.py
+++ cricinfo.py
@@ -41,15 +41,7 @@ for url in urls:
 # Get data on No. 3s and 4s
 ###############################################################################
 
-# url1 = 'https://www.espncricinfo.com/series/19430/scorecard/1187008/india-vs-south-africa-2nd-test-icc-world-test-championship-2019-2021'
-# url2 = 'https://www.espncricinfo.com/series/19297/scorecard/1187672/new-zealand-vs-england-2nd-test-england-in-new-zealand-2019-20'
-# url3 = 'https://www.espncricinfo.com/series/19497/scorecard/1225247/england-vs-west-indies-1st-test-west-indies-in-england-2020'
-
-# matches = [Match(url1), Match(url2), Match(url3)]
-
 data = []
-# data3 = []
-# data4 = []
 
 for match in matches:
     print('Current match:', match)
@@ -80,7 +72,6 @@ for match in matches:
                         batdf.loc[3, 'B'], batdf.loc[3, 'M'], notOut, fowR, 
                         fowO, fowM]
             
-            # data3.append(innsData3)
         
             if len(batdf) >= 4:
                 fow = re.split(':|\),', match.get_fow(innings))[2]
@@ -93,15 +84,14 @@ for match in matches:
                     fowM = max(m1, m2)
                 else:
                     fowM = fowM + m3            
-                notOut = 1 if batdf.loc[3, 'Dismissal'] == 'not out'\
-                    or batdf.loc[3, 'Dismissal'] == 'retired hurt' else 0
+                notOut = 1 if batdf.loc[4, 'Dismissal'] == 'not out'\
+                    or batdf.loc[4, 'Dismissal'] == 'retired hurt' else 0
                 
                 innsData4 = [batdf.loc[4, 'Batsman'], batdf.loc[4, 'R'], 
                             batdf.loc[4, 'B'], batdf.loc[4, 'M'], notOut, fowR, 
                             fowO, fowM, match.get_total(innings)]
             else:
-                innsData4 = [np.nan] * 8
-                # data4.append(innsData4)
+                innsData4 = [np.nan] * 9
         
             innsData = innsData3 + innsData4
             data.append(innsData)
@@ -110,8 +100,6 @@ cols = ['Dates', 'Season', 'MatchNo', 'Venue', 'Innings', '3Batsman', '3R', '3B'
         '3M', '3NotOut', 'FoWR1', 'FoWO1', 'FoWM1', '4Batsman', '4R', '4B', 
         '4M', '4NotOut', 'FoWR2', 'FoWO2', 'FoWM2', 'Total']
 
-# df3 = pd.DataFrame(data3, columns=cols)
-# df4 = pd.DataFrame(data4, columns=cols)
 df = pd.DataFrame(data, columns=cols)
 
 df[['MatchNo', 'Innings', '3R', '3B', '3M', '3NotOut', 'FoWR1', 'FoWO1',  
@@ -119,5 +107,3 @@ df[['MatchNo', 'Innings', '3R', '3B', '3M', '3NotOut', 'FoWR1', 'FoWO1',
     .apply(pd.to_numeric, errors='coerce')
 
 df.to_csv('df.csv', index=False)
-# df3.to_csv('df3.csv', index=False)
-# df4.to_csv('df4.csv', index=False)
